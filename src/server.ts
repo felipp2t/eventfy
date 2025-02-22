24c9772906ef8263e64f31358721f7572f1f3be7
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-02-22 16:02:06 -0300

    ✨ feat: add Swagger documentation and type provider support to Fastify server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Eventfy API',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
-
-app.get('/', async (request, reply) => {
-  return { message: 'Hello, World!' }
-})
 
 app
   .listen({
